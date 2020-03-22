b2d0de82334f80c6b1b224d6bbda8cf3c72ab9c6
Denner Marchi <denner883@hotmail.com>
2020-03-21 22:09:46 -0300

    Adicionando Footer e serviços

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,7 +7,13 @@ const homeController = {
             '/images/banner-2.jpg',
         ];
 
-        res.render('index', { title: 'CS Learning', listaDeImagens: imagens});
+        let servicos = [
+            { nome: 'Programação', imagem: '/images/undraw_code_thinking_1jeh.svg'},
+            { nome: 'Arquitetura de computadores', imagem: '/images/undraw_abstract_x68e.svg'},
+            { nome: 'Redes', imagem: '/images/undraw_connected_world_wuay.svg'},
+        ];
+
+        res.render('index', { title: 'CS Learning', listaDeImagens: imagens, listaServicos: servicos });
     },
 
 };
